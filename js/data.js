07bf864c64036b982fd1b5c67018e022393769ee
Carlos León <Carlosleoguzman1@gmail.com>
2024-08-30 20:03:07 -0400

    cambios 30-08-2024

---
+++ js/data.js
@@ -0,0 +1,69 @@
+const data = [
+    {
+        itemName: "Project 1",
+        itemCategory: "Project title",
+        itemLink: "https://",
+        itemCopy:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus corporis laboriosam reprehenderit expedita magnam fugiat quas optio dicta totam. Culpa sed tempore fugit magni fugiat commodi ut aliquam obcaecati quos?",
+        itemImg: "./images/gatete.jpg",
+    },
+    {
+        itemName: "Project 2",
+        itemCategory: "Project title",
+        itemLink: "https://",
+        itemCopy:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus corporis laboriosam reprehenderit expedita magnam fugiat quas optio dicta totam. Culpa sed tempore fugit magni fugiat commodi ut aliquam obcaecati quos?",
+        itemImg: "./images/gatete.jpg",
+    },
+    {
+        itemName: "Project 3",
+        itemCategory: "Project title",
+        itemLink: "https://",
+        itemCopy:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus corporis laboriosam reprehenderit expedita magnam fugiat quas optio dicta totam. Culpa sed tempore fugit magni fugiat commodi ut aliquam obcaecati quos?",
+        itemImg: "./img-3.jpg",
+    },
+    {
+        itemName: "Project 4",
+        itemCategory: "Project title",
+        itemLink: "https://",
+        itemCopy:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus corporis laboriosam reprehenderit expedita magnam fugiat quas optio dicta totam. Culpa sed tempore fugit magni fugiat commodi ut aliquam obcaecati quos?",
+        itemImg: "./img-4.jpg",
+    },
+    {
+        itemName: "Project 5",
+        itemCategory: "Project title",
+        itemLink: "https://",
+        itemCopy:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus corporis laboriosam reprehenderit expedita magnam fugiat quas optio dicta totam. Culpa sed tempore fugit magni fugiat commodi ut aliquam obcaecati quos?",
+        itemImg: "./img-5.jpg",
+    },
+    {
+        itemName: "Project 6",
+        itemCategory: "Project title",
+        itemLink: "https://",
+        itemCopy:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus corporis laboriosam reprehenderit expedita magnam fugiat quas optio dicta totam. Culpa sed tempore fugit magni fugiat commodi ut aliquam obcaecati quos?",
+        itemImg: "./img-1.jpg",
+    },
+    {
+        itemName: "Project 7",
+        itemCategory: "Project title",
+        itemLink: "https://",
+        itemCopy:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus corporis laboriosam reprehenderit expedita magnam fugiat quas optio dicta totam. Culpa sed tempore fugit magni fugiat commodi ut aliquam obcaecati quos?",
+        itemImg: "./img-2.jpg",
+    },
+    {
+        itemName: "Project 8",
+        itemCategory: "Project title",
+        itemLink: "https://",
+        itemCopy:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus corporis laboriosam reprehenderit expedita magnam fugiat quas optio dicta totam. Culpa sed tempore fugit magni fugiat commodi ut aliquam obcaecati quos?",
+        itemImg: "./img-3.jpg",
+    },
+
+];
+
+export { data };
