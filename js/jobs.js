39d24554af492e11944335a718870d01e00161b0
Carlos León <Carlosleoguzman1@gmail.com>
2024-09-09 21:35:11 -0300

    Cambios 09-09-2024 hola

--- js/jobs.js
+++ js/jobs.js
@@ -50,3 +50,37 @@ document.addEventListener("click", (e) => {
 function isItem(target) {
     return target.closest(".item");
 }
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    cursorOutline.style.left = `${posX}px`;
+    cursorOutline.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    }, { duration: 300, fill: "forwards" });
+});
+
+const blocks = document.querySelectorAll(".block");
+const resetDuration = 300;
+
+blocks.forEach((block) => {
+    let timeoutId;
+
+    block.addEventListener("mouseover", () => {
+        clearTimeout(timeoutId);
+        block.classList.add("active");
+        timeoutId = setTimeout(() => {
+            block.classList.remove("active");
+        }, resetDuration);
+    });
+});
